6500725855cb6f9d0a763f97adcba65d87ea39d5
thiundev <93656388+Thiun@users.noreply.github.com>
2024-07-06 12:45:16 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,218 @@
+import pickle
+import numpy as np
+
+with open('datos.bin', 'rb') as file:
+    libros = pickle.load(file)
+
+with open('usuarios.bin', 'rb') as file: 
+    usuarios = pickle.load(file)
+
+def completar_con_ceros(numero):
+    numero_con_ceros = '{:013d}'.format(int(numero))
+    return numero_con_ceros
+
+def agregar_libro(titulo, autor, isbn, categoria, estado):
+    nuevo_libro = [titulo, autor, isbn, categoria, estado]
+    libros.append(nuevo_libro)
+
+def mostrar_libros():
+    print()
+    for libro in libros: 
+        print(f"Nombre: {libro[0]}, Autor: {libro[1]}, ISBN: {libro[2]}, Categoria: {libro[3]}, Estado: {libro[4]}")
+        print()
+
+def buscar_libro(titulo, autor):
+    encontrados = [libro for libro in libros if titulo.lower() in libro[0].lower() and autor.lower() in libro[1].lower()]
+    if encontrados:
+        for libro in encontrados:
+            print(f"Nombre: {libro[0]}, Autor: {libro[1]}, ISBN: {libro[2]}, Categoria: {libro[3]}, Estado: {libro[4]}")
+    else:
+        print("No se encontraron libros que coincidan con el título y autor especificados.")
+
+def prestamo(isbn, ci):
+    usuario_encontrado = False
+    libro_encontrado = False
+    for usuario in usuarios: 
+        if usuario[1] == ci: 
+            usuario_encontrado = True
+            for libro in libros: 
+                if libro[2] == isbn:
+                    if libro[4] == "Disponible": 
+                        libro[4] = f"Prestado a {usuario[0]}"
+                        usuario[2].append(libro[0])
+                        libro_encontrado = True
+                    else: 
+                        print(f"El libro ya esta en prestamo.")
+                else: 
+                    continue          
+    if usuario_encontrado: 
+        if libro_encontrado: 
+            print()
+            print("Libro prestado exitosamente.")
+        else: 
+            print()
+            print("El libro ingresado no existe.")
+    else: 
+        print("El usuario ingresado no existe.")
+
+def devolucion(ci): 
+    for usuario in usuarios:
+        if usuario[1] == ci: 
+            libros_prestados = usuario[2]
+            print("Libros en prestamo: ")
+            for libro_prestado in libros_prestados: 
+                print(libro_prestado)
+            print()
+            libro_devolucion = input("Indique el titulo del libro que quiere devolver: ")
+            for libro_prestado in libros_prestados:
+                if libro_devolucion == libro_prestado: 
+                    usuario[2].remove(libro_devolucion)
+                    for libro in libros: 
+                        if libro[0] == libro_devolucion:
+                            libro[4] = "Disponible"
+                    print("Libro devuelto exitosamente.")
+
+def eliminar_libro(titulo):
+    i = 0 
+    libro_encontrado = False
+    for libro in libros: 
+        if libro[0] == titulo: 
+            libros.pop(i)
+            libro_encontrado = True
+            for usuario in usuarios:
+                prestamos = usuario[2]
+                if titulo in prestamos: 
+                    prestamos.remove(titulo)
+        else: 
+            i += 1
+            continue
+    if libro_encontrado: 
+        print("Libro eliminado exitosamente.")
+    else: 
+        print("El libro ingresado no existe.")
+
+def mostrar_usuarios(): 
+    print()
+    for usuario in usuarios: 
+        print(f"Nombre: {usuario[0]}, Cedula: {usuario[1]}, Prestamos: {usuario[2]}")
+        print()
+
+def agregar_usuario(usuario, ci): 
+    nuevo_usuario = [usuario, ci, []]
+    usuarios.append(nuevo_usuario)
+
+def eliminar_usuario(nombre): 
+    i = 0 
+    usuario_encontrado = False
+    for usuario in usuarios: 
+        if usuario[0] == nombre: 
+            if usuario[2]: 
+                for prestamo in usuario[2]:
+                    for libro in libros: 
+                        if libro[0] == prestamo: 
+                            libro[4] = "Disponible"
+            usuarios.pop(i)
+            usuario_encontrado = True
+        else: 
+            i += 1
+            continue
+    if usuario_encontrado: 
+        print("Usuario eliminado exitosamente.")
+    else: 
+        print("El usuario ingresado no existe.")
+
+
+while True:
+        print()
+        print("----------------------------------")
+        print("         MENU PRINCIPAL")
+        print("----------------------------------")
+        print("1. Mostrar libros")
+        print("2. Agregar libro nuevo")
+        print("3. Dar libro en prestamo")
+        print("4. Devolver libro en prestamo")
+        print("5. Buscar libro")
+        print("6. Eliminar libro")
+        print("7. Gestionar usuarios")
+        print("8. Guardar cambios y salir")
+        print()
+        opcion = int(input("Ingrese la opción deseada: "))
+
+        if opcion == 1:
+            mostrar_libros()
+        elif opcion == 2:
+            print()
+            titulo = input("Indique el titulo del libro que desea agregar: ")
+            autor = input("Indique el autor del libro que desea agregar: ")
+            isbn = input("Ingrese el ISBN del libro que desea agregar (Número de hasta 13 dígitos): ")
+            if isbn.isnumeric() and len(isbn) <= 13: 
+                isbn_completo = completar_con_ceros(isbn)
+                categoria = input("Ingrese la categoría del libro que desea agregar: ")
+                estado = "Disponible"
+                agregar_libro(titulo, autor, isbn_completo, categoria, estado)
+                print()
+                print("El libro fue agregado exitosamente.")
+            else: 
+                print("El ISBN debe ser un número de hasta 13 cifras.")
+        elif opcion == 3:
+            print()
+            ci = input("Ingrese su cedula de identidad: ")
+            if ci.isnumeric(): 
+                mostrar_libros()
+                isbn = input("Ingrese el ISBN del libro que desea prestar: ")
+                if isbn.isnumeric() and len(isbn) == 13: 
+                    prestamo(isbn, ci)
+                else: 
+                    print("El ISBN ingresado no es correcto.")
+            else: 
+                print("La cedula ingresada debe ser numerica.")
+        elif opcion == 4:
+            print()
+            ci = input("Ingrese su cedula de identidad: ")
+            if ci.isnumeric(): 
+                devolucion(ci)
+        elif opcion == 5:
+            print()
+            titulo = input("Ingrese el título del libro: ")
+            autor = input("Ingrese el autor del libro: ")
+            buscar_libro(titulo, autor)
+        elif opcion == 6:
+            print()
+            titulo = input("Ingrese el titulo del libro a eliminar: ")
+            eliminar_libro(titulo)
+        elif opcion == 7:
+            while True: 
+                print()
+                print("1. Mostrar usuarios")
+                print("2. Agregar usuario")
+                print("3. Eliminar usuario")
+                print("4. Regresar al menu principal")
+
+                opcion = int(input("Ingrese la opcion deseada: "))
+                if opcion == 1: 
+                    mostrar_usuarios()
+                elif opcion == 2: 
+                    print()
+                    usuario = input("Ingrese el usuario del nuevo usuario: ")
+                    ci = input("Ingrese cedula de identidad: ")
+                    if  ci.isnumeric(): 
+                        agregar_usuario(usuario, ci)
+                    else: 
+                        print("La cedula ingresada debe ser numerica.")
+                elif opcion == 3: 
+                    print()
+                    nombre = input("Indique el usuario que desea eliminar: ")
+                    eliminar_usuario(nombre)
+                elif opcion == 4: 
+                    break
+                else: 
+                    print("Opcion no valida")
+
+        elif opcion == 8: 
+            with open('usuarios.bin', 'wb') as file: 
+                pickle.dump(usuarios, file)
+            with open('datos.bin', 'wb') as file:
+                pickle.dump(libros, file)
+            break
+        else:
+            print("Opción no válida")
